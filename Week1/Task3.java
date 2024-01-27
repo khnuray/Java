cdd0121964af47783b32fc1ec07dbf58cbc43d83
khnuray <nkhanlarli18041@ada.edu.az>
2024-01-27 13:37:14 +0400

    Task3 finsihed

---
+++ Week1/Task3.java
@@ -0,0 +1,11 @@
+class Task3 {
+    public static void main(String[] args) {
+        int a = 25; // here we used int data type as starting point
+        long b = 5L; // here we changed int to long from previous task
+        float c = 10.0f; // here we changed int to float from previous task
+        float sum = a + b + c; // as sum we use float because it is the unuversal one for thia case
+
+        System.out.println( sum );
+
+    }
+}
